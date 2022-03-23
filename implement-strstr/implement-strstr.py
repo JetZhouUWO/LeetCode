bf9b3cd2a2301fd2b5a0ac6f5c0cafe122f596c4
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-23 12:03:50 -0400

    Time: 36 ms, Memory: 13.6 MB - LeetHub

--- implement-strstr/implement-strstr.py
+++ implement-strstr/implement-strstr.py
@@ -5,9 +5,12 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
+        # if needle == "":
+        #     return 0
+        # elif (needle in haystack):
+        #         return haystack.index(needle)
+        # else: return -1
         if needle == "":
-            return 0
-        elif (needle in haystack):
-                return haystack.index(needle)
-        else: return -1
+             return 0
+        else: return haystack.find(needle)
         
