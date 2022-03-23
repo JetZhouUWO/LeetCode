1d16b9e291f94c5110ff3eb58223b74162332211
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-23 00:08:40 -0400

    Time: 16 ms (88.74%), Space: 13.2 MB (90.02%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -4,4 +4,31 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[int]
         """
-        return matrix and list(matrix.pop(0)) + self.spiralOrder(zip(*matrix)[::-1])
+        res = []
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+
+        while left < right and top < bottom:
+            # top row
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top += 1
+
+            # right col
+            for i in range(top, bottom):
+                res.append(matrix[i][right-1])
+            right -= 1
+
+            if not (left < right and top < bottom):
+                break
+
+            # bottom row
+            for i in range(right-1, left-1, -1):
+                res.append(matrix[bottom-1][i])
+            bottom -= 1
+
+            # left col
+            for i in range(bottom-1, top-1, -1):
+                res.append(matrix[i][left])
+            left += 1      
+        return res
