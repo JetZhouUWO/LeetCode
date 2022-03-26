b853ed160a4a46ba6e81321978e3cf250cba2d8b
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-25 21:01:28 -0400

    Time: 89 ms, Memory: 14.5 MB - LeetHub

---
+++ sort-array-by-parity/sort-array-by-parity.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        even = [item for item in nums if item%2 == 0]
+        odd = [item for item in nums if item%2 == 1]
+        return list(even + odd)
+        
