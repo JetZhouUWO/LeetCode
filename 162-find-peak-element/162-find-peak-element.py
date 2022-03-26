16c4800aff167d91b1739e7ccc8bbd506ede1d4e
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-26 19:30:55 -0400

    Time: 29 ms (89.86%), Space: 13.6 MB (43.46%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -9,6 +9,8 @@ class Solution(object):
             mid = (l+r)//2
             if nums[mid] < nums[mid+1]:
                 l = mid +1
+                
+            #不用mid+1因为如果mid比右边数字大, mid有可能就是最大值, 不能除开一个大值
             else: r = mid
         return l
         
