30ba25b0e6dd05e69040210528807e912a59f122
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-26 00:49:23 -0400

    Time: 241 ms, Memory: 14.5 MB - LeetHub

---
+++ binary-search/binary-search.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # 基本判断
+        l,r = 0,len(nums)-1
+        while l<=r: 
+  
+            mid = (l+r)//2 # 也可以 l + (r-l)//2
+  
+        # 元素整好的中间位置
+            if nums[mid] == target: 
+                return mid 
+          
+        # 元素小于中间位置的元素，只需要再比较左边的元素
+            elif nums[mid] > target: 
+                r = mid-1
+  
+        # 元素大于中间位置的元素，只需要再比较右边的元素
+            else: 
+                l = mid+1
+
+        return -1
+        
