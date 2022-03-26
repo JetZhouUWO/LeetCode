6d94eb5edcf7e663a418c2cb060633f7e76e8831
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-25 20:05:21 -0400

    Time: 173 ms (81.33%), Space: 14.7 MB (49.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.py
+++ 941-valid-mountain-array/941-valid-mountain-array.py
@@ -4,20 +4,37 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-        # walk up #stops at A[i] >= A[i+1]
-        N = len(arr)
-        i = 0
-        while i+1 < N and arr[i] < arr[i+1]:
-            i += 1
+        
+#         #单指针
+#         # walk up #stops at A[i] >= A[i+1]
+#         N = len(arr)
+#         i = 0
+#         while i+1 < N and arr[i] < arr[i+1]:
+#             i += 1
 
-        # peak can't be first or last
-        if i == 0 or i == N-1:
+#         # peak can't be first or last
+#         if i == 0 or i == N-1:
+#             return False
+
+#         # walk down #starts to walk down if A[i] > A[i+1]
+#         while i+1 < N and arr[i] > arr[i+1]:
+#             i += 1
+
+#         return i == N-1
+    
+        #双指针
+        l = 0
+        r = len(arr)-1
+        
+        #左边走到停止
+        if len(arr) <3:
             return False
-
-        # walk down #starts to walk down if A[i] > A[i+1]
-        while i+1 < N and arr[i] > arr[i+1]:
-            i += 1
-
-        return i == N-1
+        while (l < (len(arr)-1)) and (arr[l] < arr[l+1]):
+            l+=1
+        while (r > 0) and (arr[r] < arr[r-1]):
+            r-=1
+        if (l == 0) or (r == len(arr)-1):
+            return False
+        return l==r
         
         
