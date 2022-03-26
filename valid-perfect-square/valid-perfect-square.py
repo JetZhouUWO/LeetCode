0cf315e7ccb2e7e1ba1091bc2715834483a7474b
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-26 19:56:39 -0400

    Time: 25 ms, Memory: 13.2 MB - LeetHub

---
+++ valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 1:
+            return  True
+        #arr = [i for i in range(1,num)]
+        l, r= 1,num
+        while l<=r:
+            mid = l + (r-l)//2
+            if mid**2 == num:
+                return True
+            elif mid**2 < num:
+                l = mid+1
+            else: r = mid-1
+        return False
+            
