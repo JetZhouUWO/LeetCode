cc29beb9096d967a5eb7bd53692bb556d4dba1ff
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-26 00:48:13 -0400

    Time: 387 ms (8.19%), Space: 15 MB (8.62%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -9,7 +9,7 @@ class Solution(object):
         l,r = 0,len(nums)-1
         while l<=r: 
   
-            mid = (l + r)//2
+            mid = l + (r-l)//2 # (l+r)//2 也可以
   
         # 元素整好的中间位置
             if nums[mid] == target: 
@@ -17,11 +17,11 @@ class Solution(object):
           
         # 元素小于中间位置的元素，只需要再比较左边的元素
             elif nums[mid] > target: 
-                r = mid-1 
+                r = mid-1 #mid-1 
   
         # 元素大于中间位置的元素，只需要再比较右边的元素
             else: 
-                l = mid+1
+                l = mid+1 #mid+1
 
         return -1
         
