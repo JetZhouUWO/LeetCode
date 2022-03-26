76f077c1cef8a99e6b287aa0e2d749bc129425d0
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-25 20:39:11 -0400

    Time: 24 ms, Memory: 13.4 MB - LeetHub

--- height-checker/height-checker.py
+++ height-checker/height-checker.py
@@ -5,9 +5,13 @@ class Solution(object):
         :rtype: int
         """
         expected = sorted(heights)
-        print(expected)
-        print(heights)
-        diff = [a - b for a, b in zip(expected, heights)]
-        diff = [item for item in diff if item != 0]
-        return len(diff)
-        
+        # print(expected)
+        # print(heights)
+        # diff = [a - b for a, b in zip(expected, heights)]
+        # diff = [item for item in diff if item != 0]
+        # return len(diff)
+        count = 0
+        for i in range(len(expected)):
+            if expected[i] != heights[i]:
+                count+=1
+        return count
