33cb8711782e55c2f7ce3e328202b4d85a01e841
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-26 20:33:25 -0400

    Time: 149 ms, Memory: 14.5 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -15,14 +15,11 @@ class Solution(object):
         while j < len(nums)-1:
             if nums[j] == nums[j+1]:
                 del nums[j+1]
+                # update一下长度
                 N = len(nums) -1
-                #print('del之后 {}'.format(nums))
-                #nums.append(-1000)
-                #print('append之后 {}'.format(nums))
             elif nums[j] != nums[j+1]:
                 j+=1
                 count +=1
-                #print('基本在这停了{},count是{}'.format(nums,count))
             elif nums[j] == -1000:
                 count-=1
                 return count
