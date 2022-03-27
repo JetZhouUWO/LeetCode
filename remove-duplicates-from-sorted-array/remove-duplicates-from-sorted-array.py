46ac422e4b98acc59a954fc9a4084626e39ee4b6
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-26 20:33:04 -0400

    Time: 136 ms, Memory: 14.7 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,24 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        #尝试使用双指针
         count = 1
         j = 0
         if len(nums) == 1:
             return count
         if len(nums) == len(set(nums)):
             return len(nums)
-        for _ in range(len(nums)):
+        N = len(nums) -1
+        while j < len(nums)-1:
             if nums[j] == nums[j+1]:
                 del nums[j+1]
+                N = len(nums) -1
                 #print('del之后 {}'.format(nums))
-                nums.append(-1000)
+                #nums.append(-1000)
                 #print('append之后 {}'.format(nums))
-            elif nums[j] > nums[j+1]:
-                pass
-                #print('基本在这停了{},count是{}'.format(nums,count))
-            else:
+            elif nums[j] != nums[j+1]:
                 j+=1
-                count+=1
-            
-        return count
+                count +=1
+                #print('基本在这停了{},count是{}'.format(nums,count))
+            elif nums[j] == -1000:
+                count-=1
+                return count
         
