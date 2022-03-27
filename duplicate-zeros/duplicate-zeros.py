4e52dcc9c011b42ff675c5de155c7eaafe7290f4
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-26 20:47:51 -0400

    Time: 60 ms, Memory: 13.8 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.py
+++ duplicate-zeros/duplicate-zeros.py
@@ -5,14 +5,12 @@ class Solution(object):
         :rtype: None Do not return anything, modify arr in-place instead.
         """
         j = 0
-        N = len(arr)
-        while j < N:
+        while j < len(arr):
             if arr[j]!=0:
                 j+=1
             else:
                 arr.insert(j,0)
                 del arr[-1]
                 j+=2
-                #N+=1
         return arr
         
