9d4470e5ede0ecadf771a2582c750fb2155e8e2b
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-26 20:35:03 -0400

    Time: 182 ms (14.54%), Space: 14.2 MB (98.78%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #尝试使用双指针
+        count = 1
+        j = 0
+        if len(nums) == 1:
+            return count
+        if len(nums) == len(set(nums)):
+            return len(nums)
+        N = len(nums) -1
+        while j < len(nums)-1:
+            if nums[j] == nums[j+1]:
+                del nums[j+1]
+                # update一下长度
+                N = len(nums) -1
+            elif nums[j] != nums[j+1]:
+                j+=1
+                count +=1
+            elif nums[j] == -1000:
+                count-=1
+                return count
+        
