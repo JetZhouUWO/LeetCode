d4281547497146bfea444c595f6c0d4138b276ab
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-26 20:23:24 -0400

    Time: 68 ms, Memory: 13.4 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -4,20 +4,37 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-        for i in range(len(arr)):
-            if (arr[i] == 0):
-                if 0 in arr[i+1:]:
-                    return True
-                    break
-                else:
-                    pass
-            elif arr[i]%2 == 0:
-                if ((2*arr[i] in arr) | (arr[i]//2 in arr)):
-                    return True
-                    break
-            else:
-                if (2*arr[i] in arr):
-                    return True
-                    break
+        # for i in range(len(arr)):
+        #     if (arr[i] == 0):
+        #         if 0 in arr[i+1:]:
+        #             return True
+        #             break
+        #         else:
+        #             pass
+        #     elif arr[i]%2 == 0:
+        #         if ((2*arr[i] in arr) | (arr[i]//2 in arr)):
+        #             return True
+        #             break
+        #     else:
+        #         if (2*arr[i] in arr):
+        #             return True
+        #             break
+        # return False
+        
+        #超过一个0直接return True
+        if arr.count(0) > 1:
+                return True
+        
+        i = 0
+        
+        arr_set = set(arr)
+        
+        while i < len(arr):
+            
+            if (arr[i] != 0) and (arr[i]*2 in arr_set):
+                return True
+            
+            i += 1
+        
         return False
         
