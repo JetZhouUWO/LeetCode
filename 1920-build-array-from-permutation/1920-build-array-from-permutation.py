429f15ba0294b6f589ddd3bd9c7679fa6618c307
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-29 19:44:36 -0400

    Time: 135 ms (44.42%), Space: 13.8 MB (31.29%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -4,10 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        # O(n) solution
         rst = []
         i = 0
         while i < len(nums):
             rst.append(nums[nums[i]])
             i+=1
         return rst
+    
+        # O(1) solution
+        i = 0
+        while i < len(nums):
+            nums[i],nums[nums[i]] = nums[nums[i]], nums[i]
+            i+=1
+        return nums
         
