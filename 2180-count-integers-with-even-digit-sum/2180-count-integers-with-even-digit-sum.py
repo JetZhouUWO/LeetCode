ee03da0243f06701f28fc15cc3362ee86884ffd0
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-29 16:14:47 -0400

    Time: 58 ms (35.48%), Space: 13.3 MB (88.69%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.py
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.py
@@ -4,9 +4,10 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        count = 0
-        for item in range(1,num+1):
-            if sum(int(ch) for ch in str(item)) % 2 == 0:
-                count+=1
-        return count
+        # count = 0
+        # for item in range(1,num+1):
+        #     if sum(int(ch) for ch in str(item)) % 2 == 0:
+        #         count+=1
+        # return count
+        return sum(sum(int(ch) for ch in str(item)) % 2 == 0 for item in range(1,num+1))
         
