ad8c6c32f6df5d3c4cd0f8cfa9ea323b96952b45
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-29 16:14:12 -0400

    Time: 61 ms (31.38%), Space: 13.2 MB (88.69%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.py
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.py
@@ -6,10 +6,7 @@ class Solution(object):
         """
         count = 0
         for item in range(1,num+1):
-            #print(item)
-            #print(sum(int(ch) for ch in str(item)))
             if sum(int(ch) for ch in str(item)) % 2 == 0:
                 count+=1
-                #print(item)
         return count
         
