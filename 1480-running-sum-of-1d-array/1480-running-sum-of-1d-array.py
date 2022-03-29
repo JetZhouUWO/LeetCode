097d91f919dfd9daae2506bd81fdbecf4db77644
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-29 19:59:33 -0400

    Time: 24 ms (90.95%), Space: 13.6 MB (63.10%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -4,10 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        total = nums[0]
-        i = 1
-        while i < len(nums):
-            nums[i] = total + nums[i]
-            total = nums[i]
-            i+=1
+        # total = nums[0]
+        # i = 1
+        # while i < len(nums):
+        #     nums[i] = total + nums[i]
+        #     total = nums[i]
+        #     i+=1
+        # return nums
+        
+        # i = 1
+        # while i<len(nums):
+        #     nums[i]+=nums[i-1]
+        #     i+=1
+        # return nums
+    
+        for i in range(1,len(nums)):
+            nums[i] +=nums[i-1]
         return nums
