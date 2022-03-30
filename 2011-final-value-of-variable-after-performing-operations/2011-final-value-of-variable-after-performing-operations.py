81cc833ab944a3cbcfc2e683f4214d238124fd7e
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-29 20:13:05 -0400

    Time: 52 ms (41.89%), Space: 13.3 MB (86.31%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type operations: List[str]
         :rtype: int
         """
-        for i in range(len(operations)):
-            operations[i] = -1 if "--" in operations[i] else 1
-        return sum(operations)
+        # for i in range(len(operations)):
+        #     operations[i] = -1 if "--" in operations[i] else 1
+        # return sum(operations)
+        
+        # True = +1 in python
+        return sum(('+' in s) - ('-' in s) for s in operations)
