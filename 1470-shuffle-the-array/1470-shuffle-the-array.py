884b7a79b644e9d82f3924f1b0b133ff76f32ad0
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-29 20:47:30 -0400

    Time: 70 ms (32.10%), Space: 13.4 MB (90.36%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def shuffle(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        res = []
+        for i, j in zip(nums[:n],nums[n:]):
+            res += [i,j]
+        return res
