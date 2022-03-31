25100396747c68c86e26b79abcea08fa50bdf722
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-30 21:50:04 -0400

    Time: 24 ms (47.63%), Space: 13.4 MB (62.89%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -10,13 +10,13 @@ class Solution(object):
         # else: return self.climbStairs(n-1) + self.climbStairs(n-2)
         
         # O(n) space答案
-        if n == 1:
-            return 1
-        res = [0 for i in range(n)] # [0 for i in xrange(n)]
-        res[0], res[1] = 1, 2
-        for i in range(2, n): # for i in xrange(2, n)
-            res[i] = res[i-1] + res[i-2]
-        return res[-1]
+        # if n == 1:
+        #     return 1
+        # res = [0 for i in range(n)] # [0 for i in xrange(n)]
+        # res[0], res[1] = 1, 2
+        # for i in range(2, n): # for i in xrange(2, n)
+        #     res[i] = res[i-1] + res[i-2]
+        # return res[-1]
     
         # constant space answer
         if n == 1:
