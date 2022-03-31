d55bd44f942f055279b7b2eab5a1434254adc7b0
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-30 22:14:35 -0400

    Time: 144 ms (59.73%), Space: 16.3 MB (15.98%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -4,17 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums = sorted(nums)
-        if len(nums) == 1:
-            return nums[0]
-        i = 0
-        while i <= len(nums)//2:
-            if i == len(nums)//2:
-                return nums[2*i]
-            elif nums[2*i] == nums[2*i+1]:
-                print("Passing number",nums[2*i])
-                i+=1
-            else:
-                return nums[2*i]
+        
+        #自己写的
+        # nums = sorted(nums)
+        # if len(nums) == 1:
+        #     return nums[0]
+        # i = 0
+        # while i <= len(nums)//2:
+        #     if i == len(nums)//2:
+        #         return nums[2*i]
+        #     elif nums[2*i] == nums[2*i+1]:
+        #         print("Passing number",nums[2*i])
+        #         i+=1
+        #     else:
+        #         return nums[2*i]
+            
+        return 2*sum(set(nums))-sum(nums)
         
         
