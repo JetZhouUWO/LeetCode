50ff1a528a11c338eb1021b721f30f5e5c1669a6
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-31 00:04:41 -0400

    Time: 135 ms (30.93%), Space: 13.2 MB (90.44%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -7,5 +7,6 @@ class Solution(object):
         if n<1:
             return False
         while n%3 == 0:
-            n = n//3
+            n = n//3 #这个相当于loop了 #smart #如果出现了不等于0的, 然后n!=1就是不是3的倍数
         return n==1
+    
