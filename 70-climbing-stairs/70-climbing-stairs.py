32cff8f03444ed253e16e88f95ae3b59ecf32869
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-30 21:49:56 -0400

    Time: 16 ms (86.56%), Space: 13.2 MB (86.26%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -4,16 +4,27 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # Method does not work due to time limit exceeded
+        # Method does not work due to time limit exceeded (应该是可以用的)
         # if n <= 3:
         #     return n
         # else: return self.climbStairs(n-1) + self.climbStairs(n-2)
         
+        # O(n) space答案
         if n == 1:
             return 1
-        res = [0 for i in range(n)]
+        res = [0 for i in range(n)] # [0 for i in xrange(n)]
         res[0], res[1] = 1, 2
-        for i in range(2, n):
+        for i in range(2, n): # for i in xrange(2, n)
             res[i] = res[i-1] + res[i-2]
         return res[-1]
+    
+        # constant space answer
+        if n == 1:
+            return 1
+        a,b = 1,2
+        for _ in range(2,n):
+            tmp = b
+            b = a+b
+            a=tmp
+        return b
         
