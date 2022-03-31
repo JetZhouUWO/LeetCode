e3e1abe6028f8bb58dc973b17a9e1a4f6426e4d8
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-30 22:30:39 -0400

    Time: 43 ms (78.25%), Space: 14.6 MB (59.26%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -5,18 +5,22 @@ class Solution(object):
         :rtype: bool
         """
         #使用双指针识别
-        l,r = 0,len(s)-1
-        print(l,r)
-        while l<r:
-            if not s[l].isalnum():
-                l+=1
-            elif not s[r].isalnum():
-                r-=1
-            else:
-                if s[l].lower() == s[r].lower():
-                    l+=1
-                    r-=1
-                else:
-                    return False
-        return True
+        # l,r = 0,len(s)-1
+        # print(l,r)
+        # while l<r:
+        #     if not s[l].isalnum():
+        #         l+=1
+        #     elif not s[r].isalnum():
+        #         r-=1
+        #     else:
+        #         if s[l].lower() == s[r].lower():
+        #             l+=1
+        #             r-=1
+        #         else:
+        #             return False
+        # return True
+    
+        #单行, idea相同
+        s = ''.join(ch for ch in s if ch.isalnum()).lower()
+        return s==s[::-1]
         
