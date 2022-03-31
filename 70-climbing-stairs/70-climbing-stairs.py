b6682025f5bcca63547be6b6df14478f5361339b
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2022-03-30 21:43:04 -0400

    Time: 27 ms (35.72%), Space: 13.3 MB (62.89%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # Method does not work due to time limit exceeded
+        # if n <= 3:
+        #     return n
+        # else: return self.climbStairs(n-1) + self.climbStairs(n-2)
+        if n == 1:
+            return 1
+        res = [0 for i in xrange(n)]
+        res[0], res[1] = 1, 2
+        for i in xrange(2, n):
+            res[i] = res[i-1] + res[i-2]
+        return res[-1]
+        
