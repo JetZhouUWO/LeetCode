45cbd8cdb7ace90218b65e5c832535bcb0d9c37c
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2024-09-22 17:16:46 -0400

    Time: 56 ms (75.94%), Space: 17.8 MB (31.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for index1, p in enumerate(nums):
-            p2 = target - p
-            if p2 in nums[index1+1:]:
-                return [nums[:index1+1].index(p), index1+nums[index1+1:].index(p2)+1]
+        # for index1, p in enumerate(nums):
+        #     p2 = target - p
+        #     if p2 in nums[index1+1:]:
+        #         return [nums[:index1+1].index(p), index1+nums[index1+1:].index(p2)+1]
+        pairs = {}
+        for index, number in enumerate(nums):
+            if target - number in pairs:
+                return[index, pairs[target - number]]
+            pairs[number] = index
