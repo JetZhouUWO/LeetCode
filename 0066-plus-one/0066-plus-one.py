99260a99c8f675179f968ecef5ac744f9707b462
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2024-09-22 21:44:06 -0400

    Time: 27 ms (97.09%), Space: 16.5 MB (66.89%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,9 +1,25 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        # l = len(digits)
+        # k = 0
+        # for index, num in enumerate(digits):
+        #     k+= num*(10**(l-1-index))
+        # k+=1
+        # k = str(k)
+        # return [int(char) for char in k]
+
         l = len(digits)
-        k = 0
-        for index, num in enumerate(digits):
-            k+= num*(10**(l-1-index))
-        k+=1
-        k = str(k)
-        return [int(char) for char in k]
+        if digits[-1] + 1 != 10:
+            digits[-1] = digits[-1] + 1
+            return digits
+        for i in range(l-1, -1, -1):
+            if digits[i] + 1 == 10:
+                digits[i] = 0
+            else:
+                digits[i] = digits[i]+1
+                break
+        
+        if digits[0] == 0:
+            return [1] + digits
+        else:
+            return digits
