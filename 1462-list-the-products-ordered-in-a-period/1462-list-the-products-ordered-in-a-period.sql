4db2269536bad3ab6def1181405d3fb4be29dd20
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2024-09-24 15:25:04 -0400

    Time: 882 ms (15.99%), Space: 0B (100%) - LeetHub

--- 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -3,4 +3,4 @@ select product_name, sum(unit) as unit
 from Products p right join Orders o on o.product_id = p.product_id
 where order_date between "2020-02-01" and "2020-02-29"
 group by product_name 
-having sum(unit) >= 100
+having unit >= 100
