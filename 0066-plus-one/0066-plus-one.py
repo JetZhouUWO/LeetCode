c710f024d37b02a8f6fb717420e0eb607caf6846
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2024-09-24 18:19:09 -0400

    Time: 39 ms (37.87%), Space: 16.5 MB (67.37%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -8,14 +8,15 @@ class Solution:
         # k = str(k)
         # return [int(char) for char in k]
 
-        if (digits[-1] + 1) != 10:
-            digits[-1] += 1
+        if digits[-1] +1 != 10:
+            digits[-1]+=1
             return digits
         
         for i in range(len(digits)-1,-1,-1):
-            if digits[i]+1 == 10:
-                digits[i] = 0
-            else:
+            if digits[i]+1 != 10:
                 digits[i] += 1
                 return digits
+            else:
+                digits[i] = 0
         return [1] + digits
+            
