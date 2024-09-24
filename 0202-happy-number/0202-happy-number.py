b133736e4587c3d76f29e3712922404622820460
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2024-09-24 18:25:53 -0400

    Time: 35 ms (70.23%), Space: 16.4 MB (73.92%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -2,19 +2,18 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         visit = set()
 
-        def get_next_num(n):
+        def get_next_number(n):
             output = 0
             while n:
-                d1 = n % 10
-                output += d1**2
+                digit = n % 10
+                output += digit**2
                 n = n // 10
             return output
         
         while n not in visit:
             visit.add(n)
-
-            n = get_next_num(n)
-
+            n = get_next_number(n)
             if n == 1:
                 return True
+        
         return False
