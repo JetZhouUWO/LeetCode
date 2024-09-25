e8b372fd6a78e5dd48292f7a09586ae404c5bf2c
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2024-09-25 17:09:00 -0400

    Time: 1497 ms (19.68%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select s.year as year, p.product_name as product_name, s.price as price
+select product_name, year, price
 from Sales s left join Product p on s.product_id = p.product_id;
