147a9f44f11020ef4f6041782107ec76e23e6184
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2024-09-26 11:47:05 -0400

    Time: 194 ms (37.8%), Space: 18.2 MB (29.01%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -6,8 +6,8 @@ class Solution:
             mid = l + (r-l)//2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                l = mid + 1
-            else:
+            elif nums[mid] > target:
                 r = mid - 1
+            else:
+                l = mid + 1
         return -1
