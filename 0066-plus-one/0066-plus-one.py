df3ec837fef01faf09c7750892c05b339ba31dc5
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2024-09-26 11:25:37 -0400

    Time: 42 ms (17.07%), Space: 16.5 MB (23.16%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,13 +1,13 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         if digits[-1] + 1 != 10:
-            digits[-1]+= 1
+            digits[-1] += 1
             return digits
         
-        for i in range(len(digits)-1, -1, -1):
-            if digits[i] +1 != 10:
-                digits[i] += 1
+        for l in range(len(digits)-1,-1,-1):
+            if digits[l] + 1 != 10:
+                digits[l]+= 1
                 return digits
             else:
-                digits[i] = 0
+                digits[l] = 0
         return [1] + digits
