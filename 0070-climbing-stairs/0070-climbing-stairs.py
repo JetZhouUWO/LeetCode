5974c81741a827d56f1145500cf8bedaf1b14413
Jet Zhou <70212237+JetZhouUWO@users.noreply.github.com>
2024-09-29 08:51:26 -0400

    Time: 41 ms (12.3%), Space: 16.6 MB (16.9%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,6 +3,8 @@ class Solution:
         dp = [0] * (n+1)
         dp[0] = 1
         dp[1] = 1
-        for l in range(2, n+1, 1):
-            dp[l] = dp[l-1] + dp[l-2]
+        
+        for i in range(2,n + 1,1):
+            dp[i] = dp[i-1] + dp[i-2]
+            
         return dp[-1]
